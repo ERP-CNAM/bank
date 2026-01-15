71cc5cc7360d2a4463f4897d518b4c6a6a20ae8a
Vladimir9595 <vladisac@hotmail.it>
2026-01-15 09:59:23 +0100

    fix: adapt api key

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ dotenv.config()
 export const env = {
     PORT: process.env.PORT || 3000,
     CONNECT_URL: process.env.CONNECT_URL || 'http://localhost:8000',
-    CONNECT_API_KEY: process.env.CONNECT_API_KEY || 'hello',
+    CONNECT_API_KEY: process.env.CONNECT_API_KEY || 'changethis',
     SERVICE_NAME: process.env.SERVICE_NAME || 'BANK',
     SERVICE_VERSION: process.env.SERVICE_VERSION || '1.0.0',
     SERVICE_HOST: process.env.SERVICE_HOST || 'localhost',
