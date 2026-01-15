fe1efa71f545ba2586877405bbf3fbd83cbae1a1
Vladimir9595 <vladisac@hotmail.it>
2026-01-15 10:03:19 +0100

    fix: adapt env file

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,10 +2,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const env = {
-    PORT: process.env.PORT || 3000,
-    CONNECT_URL: process.env.CONNECT_URL || 'http://localhost:8000',
-    CONNECT_API_KEY: process.env.CONNECT_API_KEY || 'changethis',
-    SERVICE_NAME: process.env.SERVICE_NAME || 'BANK',
-    SERVICE_VERSION: process.env.SERVICE_VERSION || '1.0.0',
-    SERVICE_HOST: process.env.SERVICE_HOST || 'localhost',
+    PORT: process.env.PORT,
+    CONNECT_URL: process.env.CONNECT_URL,
+    CONNECT_API_KEY: process.env.CONNECT_API_KEY,
+    SERVICE_NAME: process.env.SERVICE_NAME,
+    SERVICE_VERSION: process.env.SERVICE_VERSION,
+    SERVICE_HOST: process.env.SERVICE_HOST,
 }
